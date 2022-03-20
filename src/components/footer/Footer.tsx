5f4f31f3292bfc5b50f246b2c1beb971c4b2a6c9
xicooow <xico.dova@gmail.com>
2022-03-20 12:39:25 -0300

    Simplify footer portal logic

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,32 +1,20 @@
 import ReactDOM from "react-dom";
-import { FunctionComponent, useEffect } from "react";
+import { FunctionComponent } from "react";
 
 import { DARK_THEME_CLASS_NAME } from "../../utils/constants";
 
-const element = document.createElement("div");
-element.setAttribute("class", `${DARK_THEME_CLASS_NAME} font-large`);
-const footer = document.getElementById("root-footer");
-
 const Footer: FunctionComponent = () => {
-  const unmountFC = () => {
-    footer?.removeChild(element);
-  };
-
-  const mountFC = () => {
-    footer?.appendChild(element);
-    return unmountFC;
-  };
-
-  useEffect(mountFC, []);
+  const footerDOM = document.getElementById("footer");
 
   return (
     <>
       {
+        footerDOM &&
         ReactDOM.createPortal(
-          <>
+          <div className={`${DARK_THEME_CLASS_NAME} font-large`}>
             Francisco Neto <a href="https://github.com/xicooow" target="_blank" rel="noreferrer">@Github</a> - {new Date(Date.now()).getFullYear()}
-          </>,
-          element
+          </div>,
+          footerDOM
         )
       }
     </>
