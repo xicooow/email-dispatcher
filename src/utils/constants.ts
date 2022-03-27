b17b3a69988bca2060dab06b13d9656a5159eab8
xicooow <xico.dova@gmail.com>
2022-03-27 14:05:17 -0300

    Upgrade blueprintjs to v4

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 import { TRoutes } from "../types";
 
-export const DARK_THEME_CLASS_NAME = "bp3-dark";
+export const DARK_THEME_CLASS_NAME = "bp4-dark";
 export const ROUTES_MAP: { [key in TRoutes]: string } = {
   "HOME": "/",
   "VARS": "variables",
